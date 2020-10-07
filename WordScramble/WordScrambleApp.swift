08219e32cdde54381febe11ee58c296af1e06db1
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-07 09:54:30 -0400

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Cloutier, Vincent on 2020-10-07.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
