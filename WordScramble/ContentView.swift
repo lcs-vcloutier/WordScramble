4442b2c0b2856c7e7850e9b0807a3701698c8343
Vincent Cloutier <vcloutier@lcs.on.ca>
2020-10-07 13:29:54 -0400

    Started validation function

--- WordScramble/ContentView.swift
+++ WordScramble/ContentView.swift
@@ -8,13 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var usedWords = [String]()
+    @State private var rootWord = ""
+    @State private var newWord = ""
     var body: some View {
-        if let fileURL = Bundle.main.url(forResource: "some-file", withExtension: "txt") {
-            // we found the file in our bundle!
-            if let fileContents = try? String(contentsOf: fileURL) {
-                // we loaded the file into a string!
+        NavigationView {
+            VStack {
+                TextField("Enter your word", text: $newWord)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding()
+                List(usedWords, id: \.self) {
+                    Text($0)
+                }
             }
+            .navigationBarTitle(rootWord)
         }
+    }
+    func addNewWord() {
+        // lowercase and trim the word, to make sure we don't add duplicate words with case differences
+        let answer = newWord.lowercased().trimmingCharacters(in: .whitespacesAndNewlines)
+
+        // exit if the remaining string is empty
+        guard answer.count > 0 else {
+            return
+        }
+
+        // extra validation to come
+
+        usedWords.insert(answer, at: 0)
+        newWord = ""
     }
 }
 
